64e5e87dc4afd40b3635deb9e15672751ff7b904
Leonardo Mendes <leo.mi.me@gmail.com>
2018-01-26 15:25:40 -0200

    Removing App css and default logo. Creating initial structure for components and containers. Adding products json data

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Header from "./header";
+import Product from "./product";
+
+export {Header, Product}
---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+import Cart from './cart'
+import Showcase from './showcase'
+
+export {Cart, Showcase}

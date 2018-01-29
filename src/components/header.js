ee58543feaaedede0de66dcd9235c9b7d0bb4eea
Leonardo Mendes <leo.mi.me@gmail.com>
2018-01-29 17:26:04 -0200

    feat: Header component
    Added index.css to public with basic reset to margins and width 100%.
    Created common and header sass files to header component.
    Added react-icons to use Cart icon on header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from '../stylesheets/components/header.scss'
+import MdShoppingCart from 'react-icons/lib/md/shopping-cart'
 
 const Header = () => (
-  <div> <div>Livraria Flip</div> <div>Carrinho</div></div>
+  <div className={styles.header}> 
+    <div className={styles.title}>Livraria Flip</div>
+    <MdShoppingCart className={styles.cart} />
+  </div>
 )
  
 export default Header

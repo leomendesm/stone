234ca0bde661829e0b509ede0cfa932a0e323faa
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-15 16:58:10 -0200

    feat: Redux implemented to showcase
    added provider, middlewares and store to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducers from './redux-flow/reducers'
+import {
+  createStore,
+  applyMiddleware } from 'redux'
 import { Showcase } from './containers'
 import { Header } from './components'
+  
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Showcase sectionName="Últimos lançamentos" />
-        <Showcase sectionName="Best Sellers 2018" />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header />
+          <Showcase sectionName="Últimos lançamentos" />
+          <Showcase sectionName="Best Sellers 2018" />
+        </div>
+      </Provider>
     );
   }
 }
--- src/containers/showcase/showcase.js
+++ src/containers/showcase/showcase.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react'
 import { Product } from '../../components'
 import styles from './showcase.scss'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { FetchBookList } from '../../redux-flow/reducers/showcase/action-creators'
 
 class Showcase extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-      books: {
-        title: "Rápido e Devagar - Duas Formas de Pensar",
-        description: "Mostra a capacidade do pensamento rápido e sua influência persuasiva em nossas decisões.",
-        price: 55.1,
-        image: "https://images.livrariasaraiva.com.br/imagemnet/imagem.aspx/?pro_id=4074748&qld=90&l=430&a=-1"
-      }
-    }
+  componentWillMount(){
+    this.props.FetchBookList()
   }
   render() {
     return (
       <div>
         <h2 className={styles.section}>{this.props.sectionName}</h2>
         <div className={styles.showcase}>
-          <Product img={this.state.books.image} title={this.state.books.title} price={this.state.books.price} description={this.state.books.description} />
-          <Product img={this.state.books.image} title={this.state.books.title} price={this.state.books.price} description={this.state.books.description} />
-          <Product img={this.state.books.image} title={this.state.books.title} price={this.state.books.price} description={this.state.books.description} />
+          {this.props.showcase.data.map(m =>{
+            console.log(m)
+            return (<Product img={m.volumeInfo.imageLinks.thumbnail} title={m.volumeInfo.title} price={m.saleInfo.retailPrice.amount} description={m.searchInfo.textSnippet} />)
+            }
+          )}
          </div>
       </div>
     )
@@ -33,4 +30,14 @@ Product.PropTypes = {
   sectionName: PropTypes.string.isRequired
 }
 
-export default Showcase
+const mapStateToProps = state => ({
+  showcase: state.showcase
+})
+
+const mapDispatchToProps = dispatch => ({
+  FetchBookList: () => {
+    dispatch(FetchBookList(dispatch))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Showcase)

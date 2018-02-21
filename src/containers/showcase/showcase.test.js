5b1adfcf0ad48862ad040dc386ab02202419ed4c
leonardo miraglia <leo.mi.me@gmail.com>
2018-02-21 03:04:20 -0300

    test: Showcase tests fixed
    Created a store to use with showcase

--- src/containers/showcase/showcase.test.js
+++ src/containers/showcase/showcase.test.js
@@ -2,19 +2,30 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Showcase from './showcase'
 
+import thunk from 'redux-thunk'
+import reducers from '../../redux-flow/reducers'
+import {
+  createStore,
+  applyMiddleware } from 'redux'
+
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+)
+
 describe('<Showcase />', () => {
 
   it('should have a h2 tag when sectionName passed', () => {
-    const btn = shallow(<Showcase sectionName="" />)
+    const btn = shallow(<Showcase store={store} sectionName="" />).dive()
     expect(btn.find('h2')).toHaveLength(1)
   })
   it('should have a h2 with the sectionName passed', () => {
-    const btn = shallow(<Showcase sectionName="test" />)
+    const btn = shallow(<Showcase store={store} sectionName="test" />).dive()
     expect(btn.find('h2').props().children).toBe("test")
   })
 
   it('should have a Button tag when mount', () => {
-    const btn = shallow(<Showcase />)
-    expect(btn.find('Product')).toHaveLength(3)
+    const btn = shallow(<Showcase store={store} />).dive()
+    expect(btn.find('Product')).toHaveLength(0)
   })
 })

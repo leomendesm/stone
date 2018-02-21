d312ed39ad3810cf7ac51d571fb9f6d9bdd40d57
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-21 17:56:52 -0300

    feat: Links added to Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './header.scss'
 import MdShoppingCart from 'react-icons/lib/md/shopping-cart'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <header className={styles.header}> 
-    <div className={styles.title}>Livraria Flip</div>
-    <MdShoppingCart className={styles.cart} />
+    <Link to="/" className={styles.title}>Livraria Flip</Link>
+    <Link to="/cart" className={styles.cart}><MdShoppingCart className={styles.cart} /></Link>
   </header>
 )
  

6e00653ce771756832165f946ad25a336bebe7bf
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-21 17:59:52 -0300

    feat: improved add to cart

--- src/redux-flow/reducers/cart/index.js
+++ src/redux-flow/reducers/cart/index.js
@@ -9,7 +9,11 @@ export const initialState = []
 
 const cart = (state = initialState, action) => {
   switch(action.type){
-    case ADD_PRODUCT_TO_CART: return state.concat({...action.payload, quantity: 1})
+    case ADD_PRODUCT_TO_CART: 
+      if(state.filter(item => item.id === action.payload.id).length > 0)
+        return state.map(p => p.id === action.payload.id? {...p, quantity: p.quantity + 1} : p)
+      else
+        return state.concat({...action.payload, quantity: 1})
     case REMOVE_PRODUCT_FROM_CART: return state.filter((_, index) => index !== action.payload.index)
     case INCREMENT_QUANTITY_PRODUCT_ON_CART: return state.map((p, index) => index === action.payload.index? {...p, quantity: p.quantity + 1} : p)
     case DECREMENT_QUANTITY_PRODUCT_ON_CART: return state.map((p, index) => index === action.payload.index? {...p, quantity: p.quantity - 1} : p)

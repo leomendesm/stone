3192d1c5b6b2f0dfb6eba842557ed4cb7647dde3
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-21 17:54:42 -0300

    feat: New prop to button
    added onCart prop to change visual on cart component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,21 +3,27 @@ import MdAddShoppingCart from 'react-icons/lib/md/add-shopping-cart'
 import styles from './button.scss'
 import PropTypes from 'prop-types'
 
-const Button = ({ text, clickHandler, showCart }) => (
-  <div className={styles.button} onClick={clickHandler}>
+const Button = ({ text, clickHandler, showCart, onCart }) => {
+  const buttonCss = []
+  buttonCss.push(styles.button)
+  if (onCart) buttonCss.push(styles.onCart)
+  return (
+  <div className={buttonCss.join(' ')} onClick={clickHandler}>
     {showCart && <MdAddShoppingCart className={styles.icon} />}
     {text}
   </div>
-)
+)}
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
-  clickHandler: PropTypes.func.isRequired,
-  showCart: PropTypes.bool
+  clickHandler: PropTypes.func,
+  showCart: PropTypes.bool,
+  onCart: PropTypes.bool,
 }
 
 Button.defaultProps = {
-  showCart: false
+  showCart: false,
+  onCart: false,
 }
 
 export default Button

405d9a116e84c7386a03149df603490298398620
leonardo miraglia <leo.mi.me@gmail.com>
2018-02-21 03:10:22 -0300

    feat: initial cart implemented
    connected component to redux and created cart loop

--- src/containers/cart/cart.js
+++ src/containers/cart/cart.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
+import { Product } from '../../components'
+import styles from './cart.scss'
+import { connect } from 'react-redux'
+
+import { FetchBookList } from '../../redux-flow/reducers/showcase/action-creators'
 
 class Cart extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { }
-  }
-  render() { 
-    return ( <div>
-    </div> )
+
+  render() {
+    return (
+      <div>
+        <div className={styles.showcase}>
+          {this.props.cart.map(m =>
+            <Product 
+              img={m.volumeInfo.imageLinks.thumbnail}
+              title={m.volumeInfo.title}
+              price={m.saleInfo.retailPrice.amount}
+              description={m.searchInfo.textSnippet}
+            />
+          )}
+         </div>
+      </div>
+    )
   }
 }
- 
-export default Cart
+
+
+
+const mapStateToProps = state => ({
+  cart: state.cart
+})
+
+const mapDispatchToProps = dispatch => ({
+  FetchBookList: () => {
+    dispatch(FetchBookList(dispatch))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)

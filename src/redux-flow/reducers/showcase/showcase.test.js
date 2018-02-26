80738d29e229db3f64864ca2283df0d37aeae63b
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-26 17:13:23 -0300

    test: added tests to showcase reducer
    adding tests to showcase reducer testing api data manipulation

--- src/redux-flow/reducers/showcase/showcase.test.js
+++ src/redux-flow/reducers/showcase/showcase.test.js
@@ -1,7 +1,9 @@
 import showcase, { initialState } from './index'
 
-import { FETCH_BOOK_LIST } from './actions'
-
+import { 
+  FETCH_BOOK_LIST,
+  FETCH_BOOK_LIST_SUCCESS,
+  FETCH_BOOK_LIST_FAIL } from './actions'
 
 import deepFreeze from 'deep-freeze'
 
@@ -28,5 +30,79 @@ describe('Cart reducer', () => {
       const after = []
       expect(showcase(before, action)).toEqual(after)
     })
+  })
+
+  describe('Fetch book list data', () => {
+    it('should return isFetching true when action type is FETCH_BOOK_LIST', () => {
+      const before = deepFreeze({
+        isFetching: null,
+        data: [],
+        hasError: false,
+        errorMessage: null
+      })
+
+      const action = deepFreeze({
+        type: FETCH_BOOK_LIST
+      })
+
+      const after = {
+        isFetching: true,
+        data: [],
+        hasError: false,
+        errorMessage: null
+      }
+
+      expect(showcase(before, action)).toEqual(after)
+    })
+  })
+
+  describe('Fetch book list data responses', () => {
+
+    it('should set data equal to payload and isFetching false when action type is FETCH_BOOK_LIST_SUCCESS', () => {
+      const before = deepFreeze({
+        isFetching: null,
+        data: [],
+        hasError: false,
+        errorMessage: null
+      })
+
+      const action = deepFreeze({
+        type: FETCH_BOOK_LIST_SUCCESS,
+        payload: [{name: 'book'}, { name: 'book'}]
+      })
+
+      const after = {
+        isFetching: false,
+        data: [{name: 'book'}, {name: 'book'}],
+        hasError: false,
+        errorMessage: null
+      }
+
+      expect(showcase(before, action)).toEqual(after)
+    })
+
+    it('should set errorMessage equal to payload, hasError true and isFetching false when action type is FETCH_BOOK_LIST_FAIL', () => {
+      const before = deepFreeze({
+        isFetching: null,
+        data: [],
+        hasError: false,
+        errorMessage: null
+      })
+
+      const action = deepFreeze({
+        type: FETCH_BOOK_LIST_FAIL,
+        err: 'fail'
+      })
+
+      const after = {
+        isFetching: false,
+        data: [],
+        hasError: true,
+        errorMessage: 'fail'
+      }
+
+      expect(showcase(before, action)).toEqual(after)
+    })
+
   })
 })

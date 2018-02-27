3f5fd7c47a98dc8f21d225dc56343b2dfe15865e
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-27 17:52:30 -0300

    refactor: break action of showcase to better tests

--- src/redux-flow/reducers/showcase/action-creators.js
+++ src/redux-flow/reducers/showcase/action-creators.js
@@ -8,11 +8,13 @@ import { API_URL } from '../../../utils'
 
 import axios from 'axios'
 
+export function RequestAPI(dispatch){
+  return axios.get(`${API_URL}`)
+    .then(res => dispatch({ type: FETCH_BOOK_LIST_SUCCESS, payload: res.data.items }))
+    .catch(err => dispatch({ type: FETCH_BOOK_LIST_FAIL, payload: err }))
+} 
+
 export function FetchBookList (dispatch) {
-  return dispatch => {
-    dispatch({ type: FETCH_BOOK_LIST })
-    return axios.get(`${API_URL}`)
-      .then(res => dispatch({ type: FETCH_BOOK_LIST_SUCCESS, payload: res.data.items }))
-      .catch(err => dispatch({ type: FETCH_BOOK_LIST_FAIL, payload: err }))
-  }
+  dispatch({ type: FETCH_BOOK_LIST })
+  return () => RequestAPI(dispatch)
 }

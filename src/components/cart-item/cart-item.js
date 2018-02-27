33fd9dd72d24c706005815a85725950134b7d7fd
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-27 17:34:20 -0300

    refactor: Change Cart-item to stateless component

--- src/components/cart-item/cart-item.js
+++ src/components/cart-item/cart-item.js
@@ -4,61 +4,32 @@ import { Button } from '../index'
 import PropTypes from 'prop-types'
 import { notify } from 'react-notify-toast'
 
-class CartItem extends Component {
-  constructor() {
-    super();
-    this.show = notify.createShowQueue();
-  }
-  render(){
-    const {title, price, quantity, decrementHandler, incrementHandler, removeHandler} = this.props
-    return (
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <p>{title}</p>
-        </div>
-        <div className={styles.quantity}>
-          <p>R${price}</p>
-        </div>
-        <div className={styles.quantity}>
-          <Button
-            onCart={true}
-            text='-'
-            clickHandler={decrementHandler}
-            onClick={() => { this.show("Quantia alterada!","success", 1000) }} />
-          <p>{quantity}</p>
-          <Button
-            onCart={true}
-            text='+'
-            clickHandler={incrementHandler}
-            onClick={() => { this.show("Quantia alterada!","success", 1000) }} />          
-        </div>
-        <div className={styles.quantity}>  
-          <p>R${(quantity * price).toFixed(2)}</p>
-        </div>
-        <div className={styles.quantity} onClick={() => { this.show("Produto removido do carrinho!","warning", 1000) }}>
-          <Button onCart={true} text='x' clickHandler={removeHandler} />
-        </div>
-      </div>
-    )
-  }   
-}
-
-export const CartHeader = ({title, price, quantity, decrementHandler, incrementHandler, removeHandler}) => (
+const CartItem = ({title, price, quantity, decrementHandler, incrementHandler, removeHandler}) => (
   <div className={styles.item}>
     <div className={styles.content}>
-      <p>Nome do Produto</p>
+      <p>{title}</p>
     </div>
     <div className={styles.quantity}>
-      <p>Preço Unitário</p>
+      <p>R${price}</p>
     </div>
     <div className={styles.quantity}>
-      <p>Quantidade</p>      
+      <Button
+        onCart={true}
+        text='-'
+        clickHandler={decrementHandler} 
+      />
+      <p>{quantity}</p>
+      <Button
+        onCart={true}
+        text='+'
+        clickHandler={incrementHandler}
+      />          
     </div>
     <div className={styles.quantity}>  
-      <p>Preço</p>
+      <p>R${(quantity * price).toFixed(2)}</p>
     </div>
     <div className={styles.quantity}>
-      Remover
+      <Button onCart={true} text='x' clickHandler={removeHandler} />
     </div>
   </div>
 )
@@ -80,3 +51,23 @@ CartItem.defaultProps = {
 }
 
 export default CartItem
+
+export const CartHeader = () => (
+  <div className={styles.item}>
+    <div className={styles.content}>
+      <p>Nome do Produto</p>
+    </div>
+    <div className={styles.quantity}>
+      <p>Preço Unitário</p>
+    </div>
+    <div className={styles.quantity}>
+      <p>Quantidade</p>      
+    </div>
+    <div className={styles.quantity}>  
+      <p>Preço</p>
+    </div>
+    <div className={styles.quantity}>
+      Remover
+    </div>
+  </div>
+)
